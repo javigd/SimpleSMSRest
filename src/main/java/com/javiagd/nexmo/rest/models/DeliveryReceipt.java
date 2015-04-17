5959d94c04c3aa4c0787e8038763576aeca5ebc2
javigd <javigd.dtk@gmail.com>
2015-04-17 04:26:25 +0200

    refinement

--- src/main/java/com/javiagd/nexmo/rest/models/DeliveryReceipt.java
+++ src/main/java/com/javiagd/nexmo/rest/models/DeliveryReceipt.java
@@ -1,6 +1,7 @@
 package com.javiagd.nexmo.rest.models;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories
